c678aa5bca595c4c0bf177f3912e25757435f758
Alfredo Canziani <alfredo.canziani@gmail.com>
2019-02-26 23:59:54 -0500

    Add a-projections extra notebook (curse of dim)

---
+++ extra/plot_lib.py
@@ -0,0 +1,1 @@
+../plot_lib.py
--- plot_lib.py
+++ plot_lib.py
@@ -2,11 +2,13 @@ from matplotlib import pyplot as plt
 import numpy as np
 import torch
 
+
 def set_default(figsize=(10, 10)):
     plt.style.use(['dark_background', 'bmh'])
     plt.rc('axes', facecolor='k')
     plt.rc('figure', facecolor='k')
     plt.rc('figure', figsize=figsize)
+
 
 def plot_data(X, y, d=0, auto=False, zoom=1):
     X = X.cpu()
@@ -20,6 +22,7 @@ def plot_data(X, y, d=0, auto=False, zoom=1):
     plt.axvline(0, ymin=_m, color=_c, lw=1, zorder=0)
     plt.axhline(0, xmin=_m, color=_c, lw=1, zorder=0)
 
+
 def plot_model(X, y, model):
     model.cpu()
     mesh = np.arange(-1.1, 1.1, 0.01)
@@ -31,6 +34,7 @@ def plot_model(X, y, model):
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.3)
     plot_data(X, y)
 
+
 def show_scatterplot(X, colors, title=''):
     colors = colors.cpu().numpy()
     X = X.cpu().numpy()
@@ -41,8 +45,31 @@ def show_scatterplot(X, colors, title=''):
     plt.title(title)
     plt.axis('off')
 
+
 def plot_bases(bases, width=0.04):
     bases = bases.cpu()
     bases[2:] -= bases[:2]
     plt.arrow(*bases[0], *bases[2], width=width, color=(1,0,0), zorder=10, alpha=1., length_includes_head=True)
     plt.arrow(*bases[1], *bases[3], width=width, color=(0,1,0), zorder=10, alpha=1., length_includes_head=True)
+
+
+def show_mat(mat, vect, prod, threshold=-1):
+    # Subplot grid definition
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharex=False, sharey=True,
+                                        gridspec_kw={'width_ratios':[5,1,1]})
+    # Plot matrices
+    cax1 = ax1.matshow(mat.numpy(), clim=(-1, 1))
+    ax2.matshow(vect.numpy(), clim=(-1, 1))
+    cax3 = ax3.matshow(prod.numpy(), clim=(threshold, 1))
+
+    # Set titles
+    ax1.set_title(f'A: {mat.size(0)} \u00D7 {mat.size(1)}')
+    ax2.set_title(f'a^(i): {vect.numel()}')
+    ax3.set_title(f'p: {prod.numel()}')
+
+    # Plot colourbars
+    fig.colorbar(cax1, ax=ax2)
+    fig.colorbar(cax3, ax=ax3)
+
+    # Fix y-axis limits
+    ax1.set_ylim(bottom=max(len(prod), len(vect)) - 0.5)
